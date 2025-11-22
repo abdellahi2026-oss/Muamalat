404d00c21b3ef5fa1b010ab01221c5ad98f4e1be
Abdellahi Mohamed Sidi <EtfelEcherve@gmail.com>
2025-11-22 17:48:24 +0000

    زر البحث لا يعمل

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { UserNav } from '@/components/user-nav';
@@ -16,9 +17,11 @@ import { Menu, Search } from 'lucide-react';
 import { AddTransactionDialog } from './add-transaction-dialog';
 import { Input } from './ui/input';
 import { useState } from 'react';
+import { cn } from '@/lib/utils';
 
 export function Header() {
   const [searchVisible, setSearchVisible] = useState(false);
+
   return (
     <header className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-primary px-4 text-primary-foreground backdrop-blur-sm sm:px-6 lg:px-8">
       <div className="flex items-center gap-2">
@@ -50,7 +53,10 @@ export function Header() {
             <Search className="h-5 w-5"/>
             <span className="sr-only">Search</span>
           </Button>
-           <div className={`absolute left-0 top-full mt-2 w-full md:static md:w-auto md:block ${searchVisible ? 'block' : 'hidden'}`}>
+           <div className={cn(
+            "absolute left-0 top-full mt-2 w-full md:static md:block md:w-auto",
+            !searchVisible && "hidden"
+          )}>
               <div className="relative">
                 <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
                 <Input placeholder="بحث..." className="w-full rounded-lg bg-background text-foreground pl-10 md:w-[200px] lg:w-[320px]"/>
