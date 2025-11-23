38823f491106e4a836cb3f2b742b813f02e08278
Abdellahi Mohamed Sidi <EtfelEcherve@gmail.com>
2025-11-23 02:39:57 +0000

    ليكن لون الفافيكون مثل ازرق مثل درجة زرقة لون التطبيق و شعاره

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -14,7 +14,7 @@ export default function Icon() {
         <div
             style={{
                 fontSize: 24,
-                background: '#020817',
+                background: '#05107d',
                 width: '100%',
                 height: '100%',
                 display: 'flex',
